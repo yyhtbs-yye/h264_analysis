92ca44fcd95bdc7cc0425b9df5ecf1f12da2bb5c
Yuhang Ye <74354145+yyhtbs-yye@users.noreply.github.com>
2024-05-15 01:12:38 +0100

    Update xml2jsons.py

--- xml2jsons.py
+++ xml2jsons.py
@@ -2,7 +2,7 @@ import xmltodict
 import json
 import xml.etree.ElementTree as ET
 
-xml_file_path = "meerkat_CIF_trace.xml"
+xml_file_path = "00000000_trace.xml"
 
 with open(xml_file_path, 'r') as fp:
     doc = xmltodict.parse(fp.read())
@@ -12,7 +12,7 @@ for picture in doc['AVCTrace']['Picture']:
     json_doc = json.dumps(picture, indent=4)
 
     # Path to save the JSON file
-    json_file_path = f"json_traces/meerkat_CIF_{picture_id}_trace.json"
+    json_file_path = f"json_traces/00000000_{picture_id}_trace.xml"
 
     # Save the JSON data to a file
     with open(json_file_path, 'w') as json_file:
